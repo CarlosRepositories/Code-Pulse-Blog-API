48673d0832582ab5f19a3b2b893002ff72e662ac
Carlos Teixeira <email.do.carlos3@gmail.com>
2024-02-02 00:48:47 -0300

    Testing

--- CodePulse.API/CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
+++ CodePulse.API/CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d195c637f50e89cec1d0988d70f8246f42c1a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ae635de8b9fb4d8725d98f23c881495bbe7ff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
