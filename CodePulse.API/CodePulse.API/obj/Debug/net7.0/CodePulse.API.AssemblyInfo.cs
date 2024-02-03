208665a89573423b0ce69ff9bc5a62640cec3e5f
Carlos Teixeira <email.do.carlos3@gmail.com>
2024-02-02 23:36:17 -0300

    Domain to abstract an image as class

--- CodePulse.API/CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
+++ CodePulse.API/CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ae635de8b9fb4d8725d98f23c881495bbe7ff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0aad632411c04b481ee064a7b8249f7b9950545")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
